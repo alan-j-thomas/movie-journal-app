3a3978de9fa236cb8202cb581db9a38e513246f2
alan-j-thomas <alanjthomas0104@gmail.com>
2026-05-02 20:49:24 +0530

    Deleted unnecessary frontend files

--- front-end/movie-journal-frontend-v10/note.md
+++
@@ -1,3 +0,0 @@
-- Add the search bar functionality for watchlist, so that suggestions of movies show up
-- Edit in Journals was not working(bug). Fixed 
-
--- front-end/movie-journal-frontend-v10/src/utils/formatMarkdown.js
+++
@@ -1,9 +0,0 @@
-export const formatMarkdown = (text) => {
-  return text
-    .replace(/### (.*?)(\n|$)/g, '<h3>$1</h3>')
-    .replace(/## (.*?)(\n|$)/g, '<h2>$1</h2>')
-    .replace(/# (.*?)(\n|$)/g, '<h1>$1</h1>')
-    .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
-    .replace(/\*(.*?)\*/g, '<em>$1</em>')
-    .replace(/\n/g, '<br/>');
-};
